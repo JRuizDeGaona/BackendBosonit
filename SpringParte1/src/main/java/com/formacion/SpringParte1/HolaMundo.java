a26047570bab09afa880983ed43b9fc742a322de
Javier Ruiz de Gaona Tre <javier.rdegaona@bosonit.com>
2022-05-18 16:01:29 +0200

    Probando SpringBoot

---
+++ SpringParte1/src/main/java/com/formacion/SpringParte1/HolaMundo.java
@@ -0,0 +1,64 @@
+package com.formacion.SpringParte1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundo {
+    /**
+     * Método que devuelve un Hola Mundo en SpringBoot
+     *
+     * @return Hola Mundo
+     */
+    @GetMapping("/holamundo")
+    public String HolaMundo() {
+        return ("Hola Mundo");
+    }
+
+    /**
+     * Método que recibe un nombre y devuelve un saludo
+     *
+     * @param nombre nombre de la persona que queremos saludar
+     * @return String que contiene "Hola " + el @param
+     */
+    @GetMapping("/hola/{nombre}")
+    public String HolaNombre(@PathVariable String nombre) {
+        return "Hola " + nombre;
+    }
+
+    /**
+     * Método que busca entre todos nuestros trabajadores y nos dice si el id de ese trabajdor existe
+     *
+     * @param id Identificador del trabajaodr que quermos buscar
+     * @return Un String especificando si el id existe o no
+     */
+    @GetMapping("/estrabajador/{id}")
+    public String EsTrabajador(@PathVariable int id) {
+        int[] trabajadores = new int[]{1, 2, 3, 4, 5};
+        boolean flag = false;
+
+        for (int i = 0; i < trabajadores.length; i++) {
+            if (id == trabajadores[i]) {
+                flag = true;
+                break;
+            } else {
+                flag = false;
+            }
+        }
+        if (flag) {
+            return "El trabajador con id: " + id + " sí existe";
+        } else {
+            return "No hay ningún trabajador con el id: " + id;
+        }
+    }
+
+    @GetMapping(value = {"/conid", "/conid/{id}"})
+    public String HayId(@PathVariable(required = false) String id) {
+        if (id != null) {
+            return "El ID introducido es: "+id;
+        } else {
+            return "No has introducido ID";
+        }
+    }
+}
---
+++ SpringParte1/src/test/java/com/formacion/SpringParte1/SpringParte1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.SpringParte1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParte1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
