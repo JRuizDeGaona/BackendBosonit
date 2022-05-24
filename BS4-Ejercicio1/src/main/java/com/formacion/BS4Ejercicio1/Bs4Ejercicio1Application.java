ff3c7c07a7d37b6aa1cd87ebbd51e975d06405f4
Javier Ruiz de Gaona Tre <javier.rdegaona@bosonit.com>
2022-05-24 16:32:09 +0200

    Ejercicio1 BS4

---
+++ BS4-Ejercicio1/src/main/java/com/formacion/BS4Ejercicio1/Bs4Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.formacion.BS4Ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs4Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs4Ejercicio1Application.class, args);
+	}
+
+}
---
+++ BS4-Ejercicio1/src/test/java/com/formacion/BS4Ejercicio1/Bs4Ejercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.BS4Ejercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs4Ejercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
