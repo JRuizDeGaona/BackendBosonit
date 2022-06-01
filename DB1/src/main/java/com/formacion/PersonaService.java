22ccac7a6893153eebe9cfba0a219b8f5312c4eb
Javier Ruiz de Gaona Tre <javier.rdegaona@bosonit.com>
2022-06-01 09:57:07 +0200

    Limpiando código

--- DB1/src/main/java/com/formacion/PersonaService.java
+++ DB1/src/main/java/com/formacion/PersonaService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class PersonaService {
