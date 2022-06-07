5f479f28646a465bc1579cbf5926a7ffbc87d3a1
Javier Ruiz de Gaona Tre <javier.rdegaona@bosonit.com>
2022-06-07 15:09:50 +0200

    Borrando ejercicio DB2

--- EJ3-1/src/main/java/com/formacion/application/port/DeletePersonaPort.java
+++
@@ -1,7 +0,0 @@
-package com.formacion.application.port;
-
-import com.formacion.infraestructure.exception.NotFoundException;
-
-public interface DeletePersonaPort {
-    void deletePersona (int id_persona) throws NotFoundException;
-}
--- EJ3-1/src/main/java/com/formacion/application/port/UpdatePersonaPort.java
+++
@@ -1,9 +0,0 @@
-package com.formacion.application.port;
-
-import com.formacion.infraestructure.dtos.input.PersonaInputDTO;
-import com.formacion.infraestructure.dtos.output.PersonaOutputDTO;
-import com.formacion.infraestructure.exception.NotFoundException;
-
-public interface UpdatePersonaPort {
-    PersonaOutputDTO updatePersona (int id_persona, PersonaInputDTO personaInputDTO) throws NotFoundException;
-}
