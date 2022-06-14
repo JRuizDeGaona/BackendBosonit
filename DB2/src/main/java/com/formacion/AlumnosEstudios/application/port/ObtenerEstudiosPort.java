3c995011600f6b4612895c4781828990caf6c879
Javier Ruiz de Gaona Tre <javier.rdegaona@bosonit.com>
2022-06-14 15:20:53 +0200

    EJ3-1(DB2)--CRUD Estudios

--- DB2/src/main/java/com/formacion/AlumnosEstudios/application/port/ObtenerEstudiosPort.java
+++ DB2/src/main/java/com/formacion/AlumnosEstudios/application/port/ObtenerEstudiosPort.java
@@ -1,4 +1,11 @@
 package com.formacion.AlumnosEstudios.application.port;
 
+import com.formacion.AlumnosEstudios.infraestructure.dto.output.AlumnosEstudiosOutputDTO;
+import com.formacion.Excepciones.NotFoundException;
+
+import java.util.List;
+
 public interface ObtenerEstudiosPort {
+    AlumnosEstudiosOutputDTO buscarPorId (int id_study, String outputType) throws NotFoundException;
+    List<AlumnosEstudiosOutputDTO> buscarTodos(String outputType) throws Exception;
 }
---
+++ DB2/src/main/java/com/formacion/AlumnosEstudios/infraestructure/controller/AlumnosEstudiosController.java
@@ -0,0 +1,50 @@
+package com.formacion.AlumnosEstudios.infraestructure.controller;
+
+import com.formacion.AlumnosEstudios.application.port.CreateEstudiosPort;
+import com.formacion.AlumnosEstudios.application.port.DeleteEstudiosPort;
+import com.formacion.AlumnosEstudios.application.port.ObtenerEstudiosPort;
+import com.formacion.AlumnosEstudios.application.port.UpdateEstudiosPort;
+import com.formacion.AlumnosEstudios.infraestructure.dto.input.AlumnosEstudiosInputDTO;
+import com.formacion.AlumnosEstudios.infraestructure.dto.output.AlumnosEstudiosOutputDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estudios")
+public class AlumnosEstudiosController {
+    @Autowired
+    CreateEstudiosPort createEstudiosPort;
+    @Autowired
+    DeleteEstudiosPort deleteEstudiosPort;
+    @Autowired
+    UpdateEstudiosPort updateEstudiosPort;
+    @Autowired
+    ObtenerEstudiosPort obtenerEstudiosPort;
+
+    @PostMapping("/addEstudios")
+    public AlumnosEstudiosOutputDTO addEstudios(@RequestBody AlumnosEstudiosInputDTO alumnosEstudiosInputDTO) throws Exception {
+        return createEstudiosPort.addEstudios(alumnosEstudiosInputDTO);
+    }
+
+    @DeleteMapping("/deleteEstudios/{id_study}")
+    public void deleteEstudios(@PathVariable int id_study) {
+        deleteEstudiosPort.deleteEstudios(id_study);
+    }
+
+    @PutMapping("/updateEstudios/{id_study}")
+    public AlumnosEstudiosOutputDTO updateEstudios(@RequestBody AlumnosEstudiosInputDTO alumnosEstudiosInputDTO, @PathVariable int id_study) {
+        return updateEstudiosPort.updateEstudios(id_study, alumnosEstudiosInputDTO);
+    }
+
+    @GetMapping("/buscar/{id_study}")
+    public AlumnosEstudiosOutputDTO buscarPorId (@PathVariable int id_study, @RequestParam String outputType) {
+        return obtenerEstudiosPort.buscarPorId(id_study, outputType);
+    }
+
+    @GetMapping("/buscarTodos")
+    public List<AlumnosEstudiosOutputDTO> buscarTodos (@RequestParam String outputType) throws Exception {
+        return obtenerEstudiosPort.buscarTodos(outputType);
+    }
+}
